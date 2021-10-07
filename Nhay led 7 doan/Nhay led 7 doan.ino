d578d70b81aa9cc019d9d1934a1e7a6dc4010519
Khanh Tran <khanh.tv.soen@gmail.com>
2021-10-07 19:47:31 +0700

    first commit

--- Nhay led 7 doan/Nhay led 7 doan.ino
+++ Nhay led 7 doan/Nhay led 7 doan.ino
@@ -1,6 +1,7 @@
 int clockpin = 13;
 int datapin = 12;
 int latchpin = 8;
+char LED7SEG[11] = {0xC0,0xF9,0xA4,0xB0,0x99,0x92,0x82,0xF8,0x80,0x90,0xff};
 
 void setup() {
   pinMode(clockpin, OUTPUT);
@@ -9,58 +10,64 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(latchpin, LOW);
-  shiftOut(datapin, clockpin, MSBFIRST, 0xc0); //0
-  digitalWrite(latchpin, HIGH);
-  delay(1000);
+  for(int i=0; i<11; i++) {
+    digitalWrite(latchpin, LOW);
+    shiftOut(datapin, clockpin, MSBFIRST, LED7SEG[i]); //0
+    digitalWrite(latchpin, HIGH);
+    delay(100);
+  }
+  // digitalWrite(latchpin, LOW);
+  // shiftOut(datapin, clockpin, MSBFIRST, 0xc0); //0
+  // digitalWrite(latchpin, HIGH);
+  // delay(1000);
 
-  digitalWrite(latchpin, LOW);
-  shiftOut(datapin, clockpin, MSBFIRST, 0xf9);//1
-  digitalWrite(latchpin, HIGH);
-  delay(1000);
+  // digitalWrite(latchpin, LOW);
+  // shiftOut(datapin, clockpin, MSBFIRST, 0xf9);//1
+  // digitalWrite(latchpin, HIGH);
+  // delay(1000);
 
-  digitalWrite(latchpin, LOW);
-  shiftOut(datapin, clockpin, MSBFIRST, 0xa4);//2
-  digitalWrite(latchpin, HIGH);
-  delay(1000);
+  // digitalWrite(latchpin, LOW);
+  // shiftOut(datapin, clockpin, MSBFIRST, 0xa4);//2
+  // digitalWrite(latchpin, HIGH);
+  // delay(1000);
 
-  digitalWrite(latchpin, LOW);
-  shiftOut(datapin, clockpin, MSBFIRST, 0xb0);//3
-  digitalWrite(latchpin, HIGH);
-  delay(1000);
+  // digitalWrite(latchpin, LOW);
+  // shiftOut(datapin, clockpin, MSBFIRST, 0xb0);//3
+  // digitalWrite(latchpin, HIGH);
+  // delay(1000);
 
-  digitalWrite(latchpin, LOW);
-  shiftOut(datapin, clockpin, MSBFIRST, 0x99);//4
-  digitalWrite(latchpin, HIGH);
-  delay(1000);
+  // digitalWrite(latchpin, LOW);
+  // shiftOut(datapin, clockpin, MSBFIRST, 0x99);//4
+  // digitalWrite(latchpin, HIGH);
+  // delay(1000);
 
-  digitalWrite(latchpin, LOW);
-  shiftOut(datapin, clockpin, MSBFIRST, 0x92);//5
-  digitalWrite(latchpin, HIGH);
-  delay(1000);
+  // digitalWrite(latchpin, LOW);
+  // shiftOut(datapin, clockpin, MSBFIRST, 0x92);//5
+  // digitalWrite(latchpin, HIGH);
+  // delay(1000);
 
-  digitalWrite(latchpin, LOW);
-  shiftOut(datapin, clockpin, MSBFIRST, 0x82);//6
-  digitalWrite(latchpin, HIGH);
-  delay(1000);
+  // digitalWrite(latchpin, LOW);
+  // shiftOut(datapin, clockpin, MSBFIRST, 0x82);//6
+  // digitalWrite(latchpin, HIGH);
+  // delay(1000);
 
-  digitalWrite(latchpin, LOW);
-  shiftOut(datapin, clockpin, MSBFIRST, 0xf8);//7
-  digitalWrite(latchpin, HIGH);
-  delay(1000);
+  // digitalWrite(latchpin, LOW);
+  // shiftOut(datapin, clockpin, MSBFIRST, 0xf8);//7
+  // digitalWrite(latchpin, HIGH);
+  // delay(1000);
 
-  digitalWrite(latchpin, LOW);
-  shiftOut(datapin, clockpin, MSBFIRST, 0x80);//8
-  digitalWrite(latchpin, HIGH);
-  delay(1000);
+  // digitalWrite(latchpin, LOW);
+  // shiftOut(datapin, clockpin, MSBFIRST, 0x80);//8
+  // digitalWrite(latchpin, HIGH);
+  // delay(1000);
 
-  digitalWrite(latchpin, LOW);
-  shiftOut(datapin, clockpin, MSBFIRST, 0x90);//9
-  digitalWrite(latchpin, HIGH);
-  delay(1000);
+  // digitalWrite(latchpin, LOW);
+  // shiftOut(datapin, clockpin, MSBFIRST, 0x90);//9
+  // digitalWrite(latchpin, HIGH);
+  // delay(1000);
 
-  digitalWrite(latchpin, LOW);
-  shiftOut(datapin, clockpin, MSBFIRST, 0xff);//tat
-  digitalWrite(latchpin, HIGH);
-  delay(1000);
+  // digitalWrite(latchpin, LOW);
+  // shiftOut(datapin, clockpin, MSBFIRST, 0xff);//tat
+  // digitalWrite(latchpin, HIGH);
+  // delay(1000);
 }
