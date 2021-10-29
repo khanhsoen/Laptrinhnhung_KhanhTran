5347bd206ec7b83a777c574f2252d8e38dcf3910
Khanh Tran <khanh.tv.soen@gmail.com>
2021-10-29 23:04:39 +0700

    first commit

---
+++ Bai 01 Giao tiep co ban GPIO/Bai01/Bai01 giao tiep co ban/MDK-ARM/Bai01 giao tiep co ban/startup_stm32f401xe.d
@@ -0,0 +1,1 @@
+bai01 giao tiep co ban\startup_stm32f401xe.o: startup_stm32f401xe.s
---
+++ Bai 01 Giao tiep co ban GPIO/Bai01/Bai01 giao tiep co ban/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Bai01 giao tiep co ban' 
+ * Target:  'Bai01 giao tiep co ban' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
