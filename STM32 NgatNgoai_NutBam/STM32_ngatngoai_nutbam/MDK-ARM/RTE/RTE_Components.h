129bce35f5bbfb9e978404dc949aaf324fed1258
Khanh Tran <khanh.tv.soen@gmail.com>
2021-11-05 20:59:22 +0700

    first commit

---
+++ STM32 NgatNgoai_NutBam/STM32_ngatngoai_nutbam/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32_ngatngoai_nutbam' 
+ * Target:  'STM32_ngatngoai_nutbam' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
